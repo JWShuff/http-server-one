7680e60ce34c746256964d9d6ee8e6ef0da58b76
Jack Shuff <16825267+JWShuff@users.noreply.github.com>
2021-06-20 11:11:22 -0400

    Fully collecting headers and returning to the server as a dict_obj, logic to catch blank header fields. HTTP 1 complete.

--- request.py
+++ request.py
@@ -4,7 +4,13 @@ class Request:
 
     #['GET /time HTTP/1.1', 'User-Agent: PostmanRuntime/7.28.0', 'Accept: */*', 'Postman-Token: e928f7d6-ed77-4eb9-858c-ebdd3448709c', 'Host: localhost:9292', 'Accept-Encoding: gzip, deflate, br', 'Connection: keep-alive', '', '']
     def parse_request(self, decoded_request_text):
-        print(decoded_request_text[0]) #Gotta figure out how to grab these key/value pairs to run into the object
         self.parsed_request = {}
-        self.parsed_request['uri'] = decoded_request_text[0].replace("GET ", "").replace(" HTTP/1.1", "")
+        self.parsed_request['uri'] = decoded_request_text.pop(0).replace("GET ", "").replace(" HTTP/1.1", "")
+        for item in decoded_request_text:
+            if item == "":
+                continue
+            split_item = item.split(': ')
+            print(split_item)
+            self.parsed_request[split_item[0]] = split_item[1]
+        
         print(self.parsed_request)
